ee5d790f0263eb0d4308e47f80bc20a809c6911e
Kantoro Kerimkulov <kkerimku@my.centennialcollege.ca>
2026-01-30 21:55:57 -0500

    Fix import paths for Netlify build

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Netlify will serve this folder
+    outDir: 'dist', // default
   },
 });
